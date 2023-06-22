ef58f6b236f1d54caeb0efa49c88305832c310a7
TimMedin <timmedin180@gmail.com>
2023-06-22 14:19:36 +0200

    update all files

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-function redirectToOtherSite() {
-    localStorage.setItem("redirect", "true");
-    window.location.href = "/Реистрация.html";
-  }
+const openFormBtn = document.getElementById('open-form-btn');
+const openFormBtnTwo = document.getElementById('open-form-btn-two');
+const formModal = document.getElementById('form-modal');
+const overlay = document.getElementById('overlay');
+const registrationForm = document.getElementById('registration-form');
+const successModal = document.getElementById('success-modal');
+const closeBtn = document.querySelector('.close-btn');
+
+openFormBtn.addEventListener('click', function() {
+  formModal.classList.add('show');
+  overlay.classList.add('show');
+});
+
+openFormBtnTwo.addEventListener('click', function() {
+  formModal.classList.add('show');
+  overlay.classList.add('show');
+});
+
+ 
+closeBtn.addEventListener('click', function() {
+  formModal.classList.remove('show');
+  overlay.classList.remove('show');
+});
+
+registrationForm.addEventListener('submit', function(event) {
+  event.preventDefault();
   
-  if (localStorage.getItem("redirect") === "true") {
-    localStorage.removeItem("redirect");
-    window.location.href = "/Реистрация.html";
-  }
+  // Simulating registration success
+  setTimeout(function() {
+    formModal.classList.remove('show');
+    overlay.classList.add('show');
+    successModal.classList.add('show');
+    setTimeout(function() {
+      overlay.classList.remove('show');
+      successModal.classList.remove('show');
+    }, 2200);
+  }, 1000);
+});
